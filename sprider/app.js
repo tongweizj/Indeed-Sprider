805dfb07c34f0ad7abf0c0bc422913cbd986b710
max <max@luci.ai>
2021-01-04 22:35:32 -0500

    update

--- sprider/app.js
+++ sprider/app.js
@@ -1,5 +1,14 @@
 'use strict'
 const spider = require('./controllers/fetch-indeed.js')
-
+const schedule = require('node-schedule')
 // Start.listStart()
-spider()
+
+const scheduleCronstyle = () => {
+  //每分钟的第30秒定时执行一次:
+  schedule.scheduleJob('15 15 4 * * *', () => {
+    console.log('scheduleCronstyle:' + new Date())
+    spider()
+  })
+}
+
+scheduleCronstyle()
